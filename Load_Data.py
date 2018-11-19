e317754243de3ff22ec74501c5cf29c3511a7abb
payumo <diegot1@uci.edu>
2018-11-18 20:36:57 -0800

    updated the training methods for debugging

--- Load_Data.py
+++ Load_Data.py
@@ -2,6 +2,7 @@
 #Date Modification : 10/20/2018
 #Description : This function will gather and return the data to the user
 import pandas as pd
+import numpy as np
 from sklearn.model_selection import train_test_split
 class Load_Data():
     #Load Data
@@ -15,26 +16,45 @@ class Load_Data():
         input_test_insert = data[1]
         output_train_insert = data[2]
         output_test_insert = data[3]
+        print(input_train_insert[0])
+        print(len(input_train_insert))
+        print(data_list[0])
+        # print(input_test_insert[1])
+        print(type(data_list[0]))
+        print(type(input_test_insert))
 
+        # data_list[0] = data_list[0] + input_train_insert
+        # data_list[1] = data_list[1] + input_test_insert
+        # data_list[2] = data_list[2] + output_train_inser
+        # data_list[3] = data_list[3] + output_test_insert
 
-        data_list[0] = data_list[0] + input_train_insert
-        data_list[1] = data_list[1] + input_test_insert
-        data_list[2] = data_list[2] + output_train_insert
-        data_list[3] = data_list[3] + output_test_insert
+        data_list[0] = np.concatenate((data_list[0], input_train_insert), axis=0)
+        data_list[1] = np.concatenate((data_list[1], input_test_insert), axis=0)
+        data_list[2] = np.concatenate((data_list[2], output_train_insert), axis=0)
+        data_list[3] = np.concatenate((data_list[3], output_test_insert), axis=0)
+
 
         return data_list
 
     def load_data_from_folder(self, path_list,name_list,test_size):
+        # names=['center', 'left', 'right', 'steering', 'throttle', 'reverse', 'speed']
+        count = 0
+        data_list = []
         for path in path_list:
             data = self.load_data(path,name_list, test_size)
-            self.combine_contents(data, data_list)
+            if(count == 0):
+                data_list = data.copy()
+            else:
+                self.combine_contents(data, data_list)
+            count += 1
             # data_list.append(data)
 
         return data_list
 
     def load_data(self, path, name_list,test_size):
         df = pd.read_csv(path, names = name_list,engine='python')
-        input = df[['center']].values
+        print(list(df.columns.values))
+        input = df[['center','left','right']].values
         output = df[['steering']].values
         # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=, random_state=)
         # random_state = int for random seed for splitting
--- Train_Model.py
+++ Train_Model.py
@@ -1,8 +1,26 @@
 #Author: Diego Torres
 #Date Modification : 10/20/2018
 #Description : This function will gather and return the data to the user
+import pandas as pd # data analysis toolkit - create, read, update, delete datasets
+import numpy as np #matrix math
+from sklearn.model_selection import train_test_split #to split out training and testing data
+#keras is a high level wrapper on top of tensorflow (machine learning library)
+#The Sequential container is a linear stack of layers
+from keras.models import Sequential
+#popular optimization strategy that uses gradient descent
+from keras.optimizers import Adam
+#to save our model periodically as checkpoints for loading later
+from keras.callbacks import ModelCheckpoint
+#what types of layers do we want our model to have?
+from keras.layers import Lambda, Conv2D, MaxPooling2D, Dropout, Dense, Flatten
+#helper class to define input shape and generate training images given image paths & steering angles
+from utils import INPUT_SHAPE, batch_generator
+#for command line arguments
+import argparse
+#for reading files
+import os
 class Train_Model():
-	def __init__():
+	def __init__(self):
 		print('Train_Model is created')
 
 	#generator
@@ -23,7 +41,7 @@ class Train_Model():
 		yield batch_features, batch_labels
 
 
-	def train_model(self,checkpoint_file_path,model, model_param_dict,data):
+	def train_model(self,checkpoint_file_path,model, model_param_dict,data,data_dir):
 
 		#the optimizer is what changes the weights and biases during training
 		#loss is the function to calculate the error
@@ -43,7 +61,7 @@ class Train_Model():
 		#mode matches value loss
 		#######################################################################
 		checkpoint = ModelCheckpoint(
-			filepath = checkpoint_file_path,
+			filepath = checkpoint_file_path + 'model-{epoch:03d}.h5',
 			monitor = 'value_loss',
 			verbose = 1,
 			save_best_only = True,
@@ -55,14 +73,20 @@ class Train_Model():
 		#generator makes batches of samples
 		#the batch samples are configured by the number of epochs and the batch sizes
         #Need to define features, labels, batch_size
+
+        			#batch_generator(
+        			#	model_param_dict['features'][0],
+        			#	model_param_dict['labels'][0],
+        			#	model_param_dict['batch_size'][0]
+        			#),
+
 		model.fit_generator(
-			generator(
-				model_param_dict['features'][0],
-				model_param_dict['labels'][0],
-				model_param_dict['batch_size'][0]
-			),
+            batch_generator(data_dir,data[0],data[2],model_param_dict['batch_size'][0],True)
+            ,
 			samples_per_epoch = model_param_dict['samples_per_epoch'][0],
-			nb_epoch = model_param_dict['number_epoch'][0],
+			nb_epoch = model_param_dict['nb_epoch'][0],
+            validation_data=batch_generator(data_dir, data[1], data[3], model_param_dict['batch_size'][0], False),
+            nb_val_samples=len(data[1]),#input_test [input_train, input_test, output_train, output_test]
 			verbose = 1,
 			callbacks = [checkpoint]
 		)
