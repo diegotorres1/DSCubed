631e3a13a1f5077200655fcd9b5123e4cf1eac10
Diego Torres <diegot1@uci.edu>
2019-01-28 00:14:50 +0000

    completed mach1 sensor

--- tcp_server/client_sensor.py
+++ tcp_server/client_sensor.py
@@ -1,4 +1,39 @@
 #The client for the sensor information to be sent back to the computer
 
 
+import socket
+import struct
+import time
+import io
+import numpy as np
+class client_sensor():
+    def __init__(self,server_ip, server_port):
+        client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        client_socket.connect((server_ip,server_port))
+        #we want to treat the stream as a file
+        connection = client_socket.makefile('wb')
+        print('Client Sensor')
+        try:
+            stream = io.BytesIO()
+            while(True):
+                print('streaming sensor data')
+                #sensor_start, and sensor_end
+                x = np.array([1.23, 4.3])
+                stream.write(str.encode('sensor_start') + (x.tobytes()) + str.encode('sensor_end'))
+                connection.write(struct.pack('<L',stream.tell()))
+                connection.flush()
+                #find the start of the stream
+                stream.seek(0)
+                connection.write(stream.read())
+                stream.seek(0)
+                stream.truncate()
+            connection.write(struct.pack('<L', 0))
+                
+        except Exception as e:
+            print(e)
+            print('Error HIT with Sensor')    
+        finally:
+            print('Connection is closed')
+            connection.close()
+            client_socket.close()
 
--- tcp_server/client_sensor_test.py
+++ tcp_server/client_sensor_test.py
@@ -3,16 +3,16 @@ from client_sensor import client_sensor
 import argparse
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-svip',default = '192.168.0.13',type = str, dest = 'server_ip',help = 'server_ip string')
+    parser.add_argument('-svip',default = '192.168.1.14',type = str, dest = 'server_ip',help = 'server_ip string')
     parser.add_argument('-svp',default='8000',type = int, dest = 'server_port',help='server port integer')
     parser.add_argument('-vp',default = '8006',type = int, dest = 'video_port',help='video port integer')
     parser.add_argument('-sp',default = '8005',type = int, dest = 'sensor_port',help='sensor port integer')
 
     args = parser.parse_args()
     server_ip = args.server_ip
-    video_port = args.sensor_port
+    sensor_port = args.sensor_port
 
-    s = client(server_ip, sensor_port)
+    s = client_sensor(server_ip, sensor_port)
 if __name__ == '__main__':
     main()
     print('server test completed')
